d6941e3ade5c13959dc9ec59efc72fc021480001
Beliya470 <anneanziay78@gmail.com>
2023-06-14 20:04:44 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function distanceFromHqInBlocks(pickupLocation) {
   
     if (distance <= 400) {
       return 0;
-    } else if (distance > 400 && distance <= 2000) {
+    } else if (distance <= 2000) {
       return (distance - 400) * 0.02;
-    } else if (distance > 2000 && distance <= 2500) {
+    } else if (distance <= 2500) {
       return 25;
     } else {
       return 'cannot travel that far';
