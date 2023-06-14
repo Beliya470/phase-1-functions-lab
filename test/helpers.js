fad4ea6a65eb0e8f65bb2c0cfddf84cf47e5a2bd
Beliya470 <anneanziay78@gmail.com>
2023-06-14 19:48:28 +0300

    Assignment

--- test/helpers.js
+++ test/helpers.js
@@ -1,20 +1,18 @@
-const chai = require('chai')
-global.expect = chai.expect
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
-const babel = require('babel-core');
+const chai = require('chai');
+const expect = chai.expect;
+const fs = require('fs');
+const path = require('path');
+const babel = require('@babel/core');
 
-const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
+const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8');
 
 const babelResult = babel.transformFileSync(
-  path.resolve(__dirname, '..', 'index.js'), {
-    presets: ['env']
-  }
+  path.resolve(__dirname, '..', 'index.js')
 );
 
-const src = babelResult.code
+const src = babelResult.code;
 
-jsdom({
-  html, src
-});
+global.expect = expect;
+global.document = { body: { innerHTML: html } };
+global.window = { document: global.document, src: src };
+
